c691e8886caf88f17b1c3b6144b8cf45f6a3c6bf
boarrr <inomoncatnip@hotmail.com>
2025-02-07 21:39:20 +0000

    Added lab 2

--- Java/Labs/Lab 1/src/com/lab1/test/Control.java
+++ Java/Labs/Lab 1/src/com/lab1/test/Control.java
@@ -1,18 +1,4 @@
 package com.lab1.test;
 
-/*
- * Control: A class that controls the main flow of the program and allows interaction
- * Author: Ryan Pitman
- * Date: 30-1-2024
- */
-
 public class Control {
-    public static void main(String args[]) {
-        System.out.println("First lab");
-
-        Car car1 = new Car("Ryan", "10MH15358");
-        Car car2 = new Car("Ryan", "10MH15358", "Silver", false, 100, 4);
-
-        System.out.println(car2);
-    }
 }
